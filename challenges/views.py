c3dde218f960b89973435dac05771eedd1cfa82a
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-12-08 19:31:37 +0000

    making challenges app even more dynamical

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month",
+    "february": "Run for 20 min every day",
+    "march": "Learn Django for at least 20 min every day",
+    "april": "Eat no meat for the entire month",
+    "may": "Run for 20 min every day",
+    "june": "Learn Django for at least 20 min every day",
+    "july": "Eat no meat for the entire month",
+    "august": "Run for 20 min every day",
+    "september": "Learn Django for at least 20 min every day",
+    "october": "Eat no meat for the entire month",
+    "november": "Run for 20 min every day",
+    "december": "Learn Django for at least 20 min every day"
+}
 
 # Create your views here.
 
 
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month"
-    elif month == "february":
-        challenge_text = "Run for 20 min every day"
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 min every day"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported")
