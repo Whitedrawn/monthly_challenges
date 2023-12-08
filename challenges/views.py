0933914fb2fcb9d60f1db68bbe636098289a6c04
Whitedrawn <78542265+Whitedrawn@users.noreply.github.com>
2023-12-08 15:06:06 +0000

    dynamic implementation for challenges urls and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("THIS WORKS!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month"
+    elif month == "february":
+        challenge_text = "Run for 20 min every day"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 min every day"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
